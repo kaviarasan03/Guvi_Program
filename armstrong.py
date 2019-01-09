968378a88814a14ef2db82dbadf6018a67bbd6f8
kaviarasan03 <40619737+kaviarasan03@users.noreply.github.com>
2019-01-09 10:28:26 +0000

    Update armstrong.py

--- armstrong.py
+++ armstrong.py
@@ -1,12 +1,12 @@
-n=int(input())
+n1=int(input())
+n=n1
 sum1=0
 while n>0:
     r=n%10
-    n=int(n/10)
+    n=n//10
     r=r*r*r
     sum1=sum1+r
-print(sum1)    
-if n==sum1:
+if int(n1)==int(sum1):
     print("yes")
 else:
     print("no")
