89940d7282131d6c4feadd48a0fa475fb9aff72c
kaviarasan03 <40619737+kaviarasan03@users.noreply.github.com>
2019-01-09 08:54:49 +0530

    Update odd_between_two_num.py

--- odd_between_two_num.py
+++ odd_between_two_num.py
@@ -1,5 +1,9 @@
 n1,n2=map(int,input().split( ))
+li=[]
 for j in range(n1+1,n2):
 	if j%2==1:
-		print(j,end=" ")
+		li.append(j)
+for i in range(len(li)-1):
+    print(li[i],end=" ")
+print(li[len(li)-1]) 		
 	
