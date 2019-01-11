01c2038aa19fb0ad4104c5b35a4d85f327f1a73e
kaviarasan03 <40619737+kaviarasan03@users.noreply.github.com>
2019-01-11 14:07:46 +0530

    Update begin58.py

--- begin58.py
+++ begin58.py
@@ -3,6 +3,12 @@ li=list(map(int,input().split( )))
 c=0
 for i in li:
     if i==n2:
-        print("yes")
+        c=c+1
         break
+    else:
+        c=0
+if c==1:
+    print("yes")
+else:
+    print("no")
         
