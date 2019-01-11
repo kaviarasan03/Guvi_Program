f8ed6f5baa8826137cadb967a281f9f54ab38677
kaviarasan03 <40619737+kaviarasan03@users.noreply.github.com>
2019-01-11 21:15:59 +0530

    Create player14.py

---
+++ player14.py
@@ -0,0 +1,11 @@
+st=input()
+s1=st[::-1]
+li=[]
+li1=["a","e","i","o","u"]
+for i in s1:
+    if i not in li1:
+        li.append(i)
+ 
+for i in range(len(li)-1):
+	print(li[i],end="")
+print(li[len(li)-1])	
