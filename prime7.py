f868fe0aafd3aaa0af61370aa12bdc4c1c7ad9b2
kaviarasan03 <40619737+kaviarasan03@users.noreply.github.com>
2019-01-11 22:00:26 +0530

    Rename prime.py to prime7.py

---
+++ prime7.py
@@ -0,0 +1,13 @@
+num=int(input())
+if num>1:
+    for j in range(2,num):
+        if num%j==0:
+            
+            print("no")
+            break
+    else:
+        print("yes")
+else:
+    print("no")
+    
+        
