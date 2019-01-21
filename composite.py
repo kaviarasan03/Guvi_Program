0069bd99cd0c17945abdec10560c138c85422dc9
kaviarasan03 <40619737+kaviarasan03@users.noreply.github.com>
2019-01-21 19:40:00 +0530

    Create composite.py

---
+++ composite.py
@@ -0,0 +1,18 @@
+num=int(input())
+
+
+
+
+if num > 1:
+   
+   for i in range(2,num):
+       if (num % i) == 0:
+           print("yes")
+       
+           break
+   else:
+       print("no")
+       
+
+else:
+   print("yes")
