f73e457c491827bc2964765928bc422c1cded022
kaviarasan03 <40619737+kaviarasan03@users.noreply.github.com>
2019-01-22 20:38:36 +0530

    Update play21.py

--- play21.py
+++ play21.py
@@ -3,5 +3,7 @@ b1,b2=map(int,input().split( ))
 c1,c2=map(int,input().split( ))
 if a3==b1==c1 or a2==b2==c2:
     print("yes")
+elif a3==a2 or b1==b2 or c1==c2:
+    print("yes")
 else:
     print("no")
