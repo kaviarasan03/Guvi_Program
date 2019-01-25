ac5569a14919ea2f7ef7f2e3386cc98c510554d6
kaviarasan03 <40619737+kaviarasan03@users.noreply.github.com>
2019-01-25 21:09:37 +0530

    Update gcd.py

--- gcd.py
+++ gcd.py
@@ -1,8 +1,9 @@
-n1,n3=map(int,input().split( ))
+n1,n2=map(int,input().split( ))
 
 for i in range(2,n1+1):
-    if n1%i==0 and n3%i==0:
+    if n1%i==0 and n2%i==0:
         print(i)
         break
+else:
+    print("1")
     
-
