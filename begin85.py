2ebba51e13d5c22adedcb5b7647a8a1f444aa1cb
kaviarasan03 <40619737+kaviarasan03@users.noreply.github.com>
2019-01-25 21:00:20 +0530

    Update begin85.py

--- begin85.py
+++ begin85.py
@@ -3,9 +3,9 @@ l1=[]
 l2=[]
 for i in range(0,len(n1)):
 	if i%2!=0:
-		l1.append(n[i])
+		l1.append(n1[i])
 	else:
-		l2.append(n[i])
+		l2.append(n1[i])
 ls1=''.join(l1)
 ls2=''.join(l2)
 print(ls2,ls1)
