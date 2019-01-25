464b826088b07264693d5f1fc4436dcd471d10c8
kaviarasan03 <40619737+kaviarasan03@users.noreply.github.com>
2019-01-25 21:36:34 +0530

    Create begin83.py

---
+++ begin83.py
@@ -0,0 +1,16 @@
+n3,o,n2=input().split( )
+n3=int(n3)
+o=str(o)
+n2=int(n2)
+if o=="+":
+	print(n3+n2)
+elif o=="-":
+	print(n3-n2)
+elif o=="/":
+	print(n3/n2)
+elif o=="*":
+	print(n3*n2)
+elif o=="%":
+	print(n3%n2)
+elif o=="//":
+	print(n3//n2)
