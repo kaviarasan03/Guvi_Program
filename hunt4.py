244e4a68984deb80fa7b297d91a9438536c51b6f
kaviarasan03 <40619737+kaviarasan03@users.noreply.github.com>
2019-01-26 19:23:02 +0530

    Create hunt4.py

---
+++ hunt4.py
@@ -0,0 +1,10 @@
+n1=int(input())
+li=list(map(int,input().split( )))
+for i in range(0,len(li)):
+    a=li[0]
+    li.remove(a)
+    if a not in li:
+        print(a)
+        break
+    li.append(a)
+    
