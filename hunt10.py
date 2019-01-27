4d71323b90c3f3a81e5e537275a9e72a6980de99
kaviarasan03 <40619737+kaviarasan03@users.noreply.github.com>
2019-01-27 09:22:13 +0530

    Create hunt10.py

---
+++ hunt10.py
@@ -0,0 +1,13 @@
+n,n2=map(int,input().split( ))
+l1=list(map(int,input().split( )))
+l2=list(map(int,input().split( )))
+c=0
+for i in l2:
+    
+    if i in l1:
+        c=c+1
+if c==len(l2):
+    print("Yes")
+else:
+    print("No")
+    
