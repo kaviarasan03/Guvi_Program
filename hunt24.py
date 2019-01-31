c4af47cfd1e8a93a421ee1df6374133e9c257048
kaviarasan03 <40619737+kaviarasan03@users.noreply.github.com>
2019-01-31 16:11:52 +0530

    Update hunt24.py

--- hunt24.py
+++ hunt24.py
@@ -1,12 +1,16 @@
-n1,k=map(int,input().split( ))
+n,k=map(int,input().split( ))
 li=list(map(int,input().split( )))
 c=0
-for i in range(0,n1-1):
-	for j in range(i+1,n1):
+for i in range(0,n-1):
+	for j in range(i+1,n):
 		if li[i]+li[j]==k:
-			print("YES")
-			c=c+1	
-	break
-if c!=1:
-	print("NO")
 			
+			c=c+1
+			
+			
+		
+
+if c!=0:
+	print("YES")
+else:
+	print("NO")
