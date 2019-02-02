77bb44d02a62e03831498445809a8d8239d51df4
kaviarasan03 <40619737+kaviarasan03@users.noreply.github.com>
2019-02-02 13:39:35 +0530

    Create pro42.py

---
+++ pro42.py
@@ -0,0 +1,29 @@
+n1,n2=map(int,input().split( ))
+li=list(map(int,input().split( )))
+li1=[]
+li2=[]
+c=0
+k1=0
+
+if n1==n2:
+    print(min(li))
+else:
+    for i in range(0,n1):
+        c=c+1
+        k1=k1+1
+        if c>n2:
+            a=min(li1)
+            li1=[]
+            li2.append(a)
+            li1.append(li[i])
+            c=1
+            
+        
+        else:
+            li1.append(li[i])
+        if k1==n1:
+            b=min(li1)
+            li2.append(b)
+        
+    print(max(li2))    
+
